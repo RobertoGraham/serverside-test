6fd934dab6c63a1c7c7823facd6f6c599953cc26
unknown <robertogrhm@gmail.com>
2019-05-06 18:27:40 +0100

    Added check for null httpclient instances passed to clientimpl constructor

--- src/main/java/robertograham/serversidetest/client/ClientImpl.java
+++ src/main/java/robertograham/serversidetest/client/ClientImpl.java
@@ -6,13 +6,14 @@ import robertograham.serversidetest.client.domain.Product;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 final class ClientImpl implements Client {
 
     private final CloseableHttpClient httpClient;
 
     ClientImpl(final CloseableHttpClient httpClient) {
-        this.httpClient = httpClient;
+        this.httpClient = Objects.requireNonNull(httpClient, "httpClient cannot be null");
     }
 
     @Override
@@ -23,5 +24,27 @@ final class ClientImpl implements Client {
     @Override
     public List<Product> getProducts() {
         return Collections.emptyList();
+    }
+
+    @Override
+    public String toString() {
+        return "ClientImpl{" +
+            "httpClient=" + httpClient +
+            '}';
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object)
+            return true;
+        if (!(object instanceof ClientImpl))
+            return false;
+        final ClientImpl client = (ClientImpl) object;
+        return httpClient.equals(client.httpClient);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(httpClient);
     }
 }
